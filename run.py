333a4689cb4946dc3ecf5f31355a60c6079bfe54
andreagorsche <andrea.gorsche@gmail.com>
2022-10-03 17:54:10 +0000

    Use RegEx to validate Austrian numberplate, if else statement for status print to user

--- run.py
+++ run.py
@@ -23,5 +23,17 @@ SHEET = GSPREAD_CLIENT.open('drivers_logbook')
 logbook = SHEET.worksheet('logbook')
 
 #get and print all values from the logbook worksheet
-data = logbook.get_all_values()
-print(data) 
+data = logbook.get_all_values() 
+
+# import Python RegEx
+import re
+
+# define valid numberplate with Reg Ex
+NUM_PLATE = input("Please enter your number plate (e.g. G-60-CYD):")
+VALID_AUT = re.search("[A-Z]{1,2}-[0-9]{1,5}-[A-Z]{1,5}$", NUM_PLATE)
+
+if VALID_AUT:
+    print("The number plate is of regular format. Entry correct.")
+else:
+    print("invalid entry, please try again")
+
